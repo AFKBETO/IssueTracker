30abfca2255609518d9b959a84400dc34da11996
QV Nguyen <qv.nguyen@gmail.com>
2022-05-01 00:25:09 +0200

    add Register Service in client

---
+++ client/src/components/RegisterService.vue
@@ -0,0 +1,41 @@
+<template>
+    <div>
+        <h1>Register</h1>
+        <form @submit.prevent="register">
+            <input 
+                type="email" 
+                placeholder="Enter your email" 
+                name="email"
+                v-model="email" /><br>
+            <input 
+                type="password"
+                placeholder="Enter your password"
+                name="password"
+                v-model="password" /><br>
+            <button type="submit">REGISTER</button>
+        </form>
+    </div>
+</template>
+
+<script>
+import AuthenticationService from '@/services/AuthenticationService'
+export default {
+    data() {
+        return {
+            email: '',
+            password: ''
+        }
+    },
+    methods: {
+        async register () {
+            const response = await AuthenticationService.register({
+                email: this.email,
+                password: this.password
+            })
+            this.email = ''
+            this.password = ''
+            console.log(response.data)
+        }
+    }
+}
+</script>
