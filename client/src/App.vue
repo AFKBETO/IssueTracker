750c8900f015b1f1dafc14c641d025e34c8a6982
QV Nguyen <qv.nguyen@gmail.com>
2022-05-01 15:27:08 +0200

    nav dynamic tab bootstrap

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,14 +1,26 @@
 <template>
   <ul class="nav justify-content-center nav-tabs">
     <li class="nav-item">
-      <router-link class="nav-link" to="/">Home</router-link>
+      <router-link class="nav-link" :class="activeClass('home')" to="/">Home</router-link>
     </li>
     <li class="nav-item">
-      <router-link class="nav-link"  to="/register">Register</router-link>
+      <router-link class="nav-link" :class="activeClass('register')" to="/register">Register</router-link>
     </li>
   </ul> 
-  <router-view class="tab-pane container active" :id="$route.name" />
+  <div class="tab-content">
+      <router-view class="tab-pane active" ></router-view>
+  </div>
 </template>
 
 <script>
+module.exports = {
+  methods: {
+      activeClass: function (...names) {
+        for (let name of names) {
+            if (name == this.$route.name)
+                return 'active';
+      }
+    }    
+}
+}
 </script>
