01fbbef1b05a9eabab7cd5c0cd8aab5e083cbfbd
AFKBETO <afk.beto@gmail.com>
2022-05-02 11:31:26 +0200

    init ProductController (create, read)

---
+++ server/src/controllers/ProductController.js
@@ -0,0 +1,50 @@
+const { Product: Project } = require('../database/models')
+const { jwtVerifyUser } = require('./VerifyController')
+const { errorHandler } = require('./ErrorHandler')
+
+module.exports = {
+    /* 
+    Creating a new project
+    Minimum permission: 2 (Project Manager)
+     */
+    async create (req, res) {
+        try {
+            const decoded = jwtVerifyUser(req, 2)
+            const project = await Project.create({
+                name: req.body.name,
+                manageByUser: decoded.id,
+                description: req.body.description
+            })
+            res.status(200).send({
+                message: "Project successfully created."
+            })
+        }
+        catch (err) {
+            const error = errorHandler(err)
+            res.status(error.status).send({
+                error: `Unable to create project: ${error.message}`
+            })
+        }
+        return
+    },
+
+    async read (req, res) {
+        try {
+            const decoded = jwtVerifyUser(req, 4)
+            const {id} = req.body
+            const project = await Project.findAll({
+                where: {
+                    manageByUser: decoded.id
+                }
+            })
+            
+
+        }
+        catch (err) {
+            const error = errorHandler(err)
+            res.status(error.status).send({
+                error: `Unable to read project: ${error.message}`
+            })
+        }
+    }
+}
