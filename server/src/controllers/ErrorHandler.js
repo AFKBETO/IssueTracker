40bd981a2b8d6ee090f85a6eb21938de1bf8fdb3
AFKBETO <afk.beto@gmail.com>
2022-05-02 13:24:48 +0200

    refactor ErrorHandler

--- server/src/controllers/ErrorHandler.js
+++ server/src/controllers/ErrorHandler.js
@@ -1,24 +1,28 @@
-export function errorHandler(err) {
-    const error = {
-        status: 400,
-        message: err.message
+module.exports = {
+    errorHandler(err) {
+        const error = {
+            status: 400,
+            message: err.message
+        }
+        switch (err.name) {
+            case "TokenExpiredError":
+                error.status = 401
+                error.message = "Your session has expired."
+                break
+            case "InvalidRole":
+            case "UnauthorizedAction":
+                error.status = 403
+                break
+            case "UserNotFound":
+            case "ProjectNotFound":
+                error.status = 404
+                break
+            case "ParticipantAlreadyAssigned":
+                error.status = 500
+                break
+            default:
+                break
+        }
+        return error
     }
-    switch (err.name) {
-        case "TokenExpiredError":
-            error.status = 401
-            error.message = "Your session has expired."
-            break
-        case "InvalidRole":
-        case "UnauthorizedAction":
-            error.status = 403
-            break
-        case "UserNotFound":
-        case "ProjectNotFound":
-            error.status = 404
-            break
-        default:
-            error.status = 500
-            break
-    }
-    return error
 }
