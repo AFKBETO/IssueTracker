efe62092f78b435bd43abab148e5003317d8e14e
AFKBETO <afk.beto@gmail.com>
2022-05-02 18:11:50 +0200

    add more error handlers

--- server/src/controllers/ErrorHandler.js
+++ server/src/controllers/ErrorHandler.js
@@ -1,3 +1,7 @@
+/*
+this function embeds error status into the error message
+*/
+
 module.exports = {
     errorHandler(err) {
         const error = {
@@ -11,6 +15,7 @@ module.exports = {
                 break
             case "InvalidRole":
             case "UnauthorizedAction":
+            case "WrongLogin":
                 error.status = 403
                 break
             case "UserNotFound":
@@ -18,6 +23,8 @@ module.exports = {
                 error.status = 404
                 break
             case "ParticipantAlreadyAssigned":
+            case "JsonWebTokenError":
+            case "NotBeforeError":
                 error.status = 500
                 break
             default:
