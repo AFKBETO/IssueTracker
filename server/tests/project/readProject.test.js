8835477a4004c50ccefc26365780b75eb2b141fc
QV NGUYEN <qv.nguyen00@gmail.com>
2022-05-04 22:15:48 +0200

    read project unit test done

--- server/tests/project/readProject.test.js
+++ server/tests/project/readProject.test.js
@@ -8,113 +8,84 @@ describe("Administrator user", async function() {
     const req = new Request()
     it("Login as admin", async function() {
         req.body = {
-            email: user.dev.email,
-            password: user.dev.password
+            email: user.admin.email,
+            password: user.admin.password
         }
-        runLogin(req, user.dev.name)
+        await runLogin(req, user.admin.name)
     })
     describe("Admin working on project", async function () {
         req.body = {}
-        it("Read all projects hey", async function() {
-            runRead(req, 1, 400)
+        it("Read all projects", async function() {
+            await runRead(req, 0, 200)
         })
-        /* req.body = {
-            name: "ProjectManager",
-            description: "A project managed by manager"
-        }
-        it("Create a new project for a manager", async function() {
-            runCreate(req, 2, 201)
+        it("Read all projects of 1", async function() {
+            await runRead(req, 1, 200)
         })
-        req.body = {
-            name: "ProjectDev",
-            description: "A project managed by a dev"
-        }
-        it("Create a new project for a non-manager, should fail with 400", async function() {
-            runCreate(req, 3, 400)
+        it("Read all projects of 2", async function() {
+            await runRead(req, 2, 200)
         })
-        req.body = {
-            name: "ProjectGhost",
-            description: "A project managed by a ghost"
-        }
-        it("Create a new project for a non-user, should fail with 404", async function() {
-            runCreate(req, 69420, 404)
-        }) */
+        it("Read all projects of 3, should fail and get 404", async function() {
+            await runRead(req, 3, 404)
+        })
     })
 })
 
-/* describe("Project Manager user", async function() {
+describe("Project Manager user", async function() {
     const req = new Request()
     it("Login as manager", async function() {
         req.body = {
             email: user.manager.email,
             password: user.manager.password
         }
-        runLogin(req, user.manager.name)
+        await runLogin(req, user.manager.name)
     })
     describe("Manager working on project", async function () {
-        req.body = {
-            name: "ProjectManager",
-            description: "A project managed by manager"
-        }
-        it("Create a new project for self", async function() {
-            runCreate(req, 0, 201)
+        req.body = {}
+        it("Read all projects", async function() {
+            await runRead(req, 0, 200)
         })
-        req.body = {
-            name: "ProjectAdmin",
-            description: "A project managed by admin"
-        }
-        it("Try to create a project for an admin, should be ignored", async function() {
-            runCreate(req, 1, 201, 2)
+        it("Read all projects of 1, should fail and get 403", async function() {
+            await runRead(req, 1, 403)
+        })
+        it("Read all projects of 2 (self), should fail and get 403 due to wrong route", async function() {
+            await runRead(req, 2, 403)
         })
     })
 })
 
 describe("Developer user", async function() {
     const req = new Request()
-    it("Login as developer", async function() {
+    it("Login as dev", async function() {
         req.body = {
             email: user.dev.email,
             password: user.dev.password
         }
-        runLogin(req, user.dev.name)
+        await runLogin(req, user.dev.name)
     })
     describe("Developer working on project", async function () {
-        req.body = {
-            name: "ProjectDev",
-            description: "A project managed by a dev"
-        }
-        it("Create a new project for self, should fail with 403", async function() {
-            runCreate(req, 0, 403)
+        req.body = {}
+        it("Read all projects, should fail and get 403", async function() {
+            await runRead(req, 0, 403)
+        })
+        it("Read all projects of 1, should fail and get 403", async function() {
+            await runRead(req, 1, 403)
         })
     })
 })
 
-async function runCreateExtra(request, target, statusCode, manageByUser) {
-    if (target) {
-        request.body["manageByUser"] = target
-    }
-    const res = new Response()
-
-    await create(request, res)
-    assert.equal(res.data.status, statusCode, `Status code should be ${statusCode}, but got ${res.data.status}: ${res.data.error}`)
-    const project = res.data.dataValues
-    assert.equal(project.name, request.body.name, `Project name should be ${request.body.name}, but got ${project.name}`)
-    assert.equal(project.manageByUser, manageByUser, `Manager should be ${manageByUser}, but got ${project.manageByUser}`)
-    assert.equal(project.description, request.body.description, `Manager should be ${request.body.description}, but got ${project.description}`)
-}
-
-async function runCreate(request, target, statusCode) {
-    runCreateExtra(request, target, statusCode, target)
-} */
-
 async function runRead(request, userid, statusCode) {
-    assert.equal(statusCode, 201, "Fail")
     if (userid) {
         request.params["userId"] = userid
     }
     const res = new Response()
     await read(request, res)
     assert.equal(res.data.status, statusCode, `Status code should be ${statusCode}, but got ${res.data.status}: ${res.data.error}`)
-    const projects = res.data.dataValues
-    assert.isBoolean(projects, `Should be an array, but got ${projects.constructor.name}`)
+    if(userid && res.data.status == 200) {
+        const keys = Object.keys(res.data)
+        for (let key of keys) {
+            if (key == "status") continue
+            assert.equal(res.data[key].manageByUser, userid, `Should not read projects of other users`)
+        }
+    }
+
 }
