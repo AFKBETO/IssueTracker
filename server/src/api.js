2dd1689d3460f82b04b2fb87b29895046bd7f69a
QV NGUYEN <qv.nguyen00@gmail.com>
2022-05-05 18:13:07 +0200

    add api route for create + read ticket

--- server/src/api.js
+++ server/src/api.js
@@ -4,6 +4,7 @@ const AuthenticationController = require('./controllers/AuthenticationController
 const AuthenticationControllerPolicy = require('./policies/AuthenticationControllerPolicy')
 const ProjectController = require('./controllers/ProjectController')
 const PartipationController = require('./controllers/ParticipationController')
+const TicketController = require('./controllers/TicketController')
 
 router.post('/register', AuthenticationControllerPolicy.register, AuthenticationController.register)
 router.post('/login', AuthenticationController.login)
@@ -17,6 +18,10 @@ router.delete('/project/:projectId', ProjectController.remove)
 router.post('/participation', PartipationController.create)
 router.get('/participation/:userId', PartipationController.read)
 router.delete('/participation', PartipationController.remove)
+
+router.post('/ticket', TicketController.create)
+router.get('/ticket/project/:idProject', TicketController.readProject)
+router.get('/ticket/user/:idUser', TicketController.readUser)
 
 router.get('/token', (req, res) => {
     console.log(req)
