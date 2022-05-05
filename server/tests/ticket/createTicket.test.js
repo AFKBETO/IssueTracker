93a3ed381a5956dd5915401d800d8dee8ed3f92c
QV NGUYEN <qv.nguyen00@gmail.com>
2022-05-05 17:48:17 +0200

    add create ticket unit test

---
+++ server/tests/ticket/createTicket.test.js
@@ -0,0 +1,82 @@
+const { assert } = require('chai')
+const { create } = require('../../src/controllers/TicketController')
+const { Request, Response } = require('../ReqRes')
+const user = require('../devinit/user.init')
+const runLogin = require('../Login')
+
+describe("Administrator user", async function() {
+    const req = new Request()
+    it("Login as admin", async function() {
+        req.body = {
+            email: user.admin.email,
+            password: user.admin.password
+        }
+        await runLogin(req, user.admin.name)
+    })
+    describe("Admin opens a ticket in a project", async function () {
+        it("Opening a ticket in project 25", async function() {
+            req.body = {
+                issueByUser: user.admin.id,
+                idProject: 25,
+                name: "AdminBug",
+                description: "A bug caused by admin"
+            }
+            await runCreate(req, 201)
+        })
+        it("Opening a ticket in project 27, should fail and get 403", async function() {
+            req.body = {
+                issueByUser: user.admin.id,
+                idProject: 27,
+                name: "AdminBug2",
+                description: "Another bug caused by admin"
+            }
+            await runCreate(req, 403)
+        })
+    })
+})
+
+describe("Submittor user", async function() {
+    const req = new Request()
+    it("Login as submittor", async function() {
+        req.body = {
+            email: user.submittor.email,
+            password: user.submittor.password
+        }
+        await runLogin(req, user.submittor.name)
+    })
+    describe("Admin opens a ticket in a project", async function () {
+        it("Opening a ticket in project 25", async function() {
+            req.body = {
+                issueByUser: user.submittor.id,
+                idProject: 25,
+                name: "SubmittorBug",
+                description: "A bug caused by submittor"
+            }
+            await runCreate(req, 201)
+        })
+        it("Opening a ticket in project 27, should fail and get 403", async function() {
+            req.body = {
+                issueByUser: user.submittor.id,
+                idProject: 27,
+                name: "SubmittorBug2",
+                description: "Another bug caused by submittor"
+            }
+            await runCreate(req, 403)
+        })
+    })
+})
+
+async function runCreate(request, statusCode) {
+    
+    const res = new Response()
+
+    await create(request, res)
+    assert.equal(res.data.status, statusCode, `Status code should be ${statusCode}, but got ${res.data.status}: ${res.data.error}`)
+    if (statusCode == 201) {
+        const ticket = res.data.dataValues
+        assert.equal(ticket.idProject, request.body.idProject, `Project id should be ${request.body.idProject}, but got ${ticket.idProject}`)
+        assert.equal(ticket.issueByUser, request.body.issueByUser, `User id should be ${request.body.issueByUser}, but got ${ticket.issueByUser}`)
+        assert.isTrue(ticket.active, `New ticket should be active (true), but got false instead.`)
+    }
+
+}
