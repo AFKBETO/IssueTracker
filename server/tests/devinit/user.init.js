3da47c0bf8b4e6acbfe7f577994b8080cac864ef
QV NGUYEN <qv.nguyen00@gmail.com>
2022-05-05 16:28:36 +0200

    add id in user init, used for other stuffs

--- server/tests/devinit/user.init.js
+++ server/tests/devinit/user.init.js
@@ -2,24 +2,28 @@ const { User } = require('../../src/database/models')
 
 const user = {
     admin: {
+        id: 1,
         name: "Administrator",
         email: "abc@def.com",
         password: "pCFbR3d9yMRuZfn_",
         role: 1
     },
     manager: {
+        id: 2,
         name: "Project Manager",
         email: "abcde@yahoo.com",
         password: "Sdf(123)",
         role: 2
     },
     dev: {
+        id: 3,
         name: "Developer",
         email: "abc@yahoo.com",
         password: "Sdf(123)",
         role: 3
     },
     submittor: {
+        id: 4,
         name: "Submittor",
         email: "abcd@yahoo.com",
         password: "Sdf(123)",
@@ -28,22 +32,22 @@ const user = {
 }
 
 User.findOrCreate({
-    where: {id: "1"},
+    where: {id: 1},
     defaults: user.admin
 })
 
 User.findOrCreate({
-    where: {id: "2"},
+    where: {id: 2},
     defaults: user.manager
 })
 
 User.findOrCreate({
-    where: {id: "3"},
+    where: {id: 3},
     defaults: user.dev
 })
 
 User.findOrCreate({
-    where: {id: "4"},
+    where: {id: 4},
     defaults: user.submittor
 })
 
