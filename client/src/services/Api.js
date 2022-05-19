4b510ce0372641aa9181f4ed927282724a5ab0ea
QV NGUYEN <qv.nguyen00@gmail.com>
2022-05-19 13:57:58 +0200

    redo all

--- client/src/services/Api.js
+++
@@ -1,9 +0,0 @@
-import axios from 'axios'
-
-export function Api() {
-    return axios.create({
-        baseURL: `http://localhost:8081/api/`
-    })
-}
-
-export const AUTH_TOKEN_KEY = 'authToken'
