b554e7144a48ec3fccd53e37dd97b35e8814a73c
QV NGUYEN <qv.nguyen00@gmail.com>
2022-05-20 14:10:26 +0200

    fix Firebase

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -6,6 +6,17 @@ import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { SharedModule } from './shared/shared.module';
 import { HomePageComponent } from './home-page/home-page.component';
+import { initializeApp,provideFirebaseApp } from '@angular/fire/app';
+import { environment } from '../environments/environment';
+import { provideAnalytics,getAnalytics,ScreenTrackingService,UserTrackingService } from '@angular/fire/analytics';
+import { provideAuth,getAuth } from '@angular/fire/auth';
+import { provideDatabase,getDatabase } from '@angular/fire/database';
+import { provideFirestore,getFirestore } from '@angular/fire/firestore';
+import { provideFunctions,getFunctions } from '@angular/fire/functions';
+import { provideMessaging,getMessaging } from '@angular/fire/messaging';
+import { providePerformance,getPerformance } from '@angular/fire/performance';
+import { provideRemoteConfig,getRemoteConfig } from '@angular/fire/remote-config';
+import { provideStorage,getStorage } from '@angular/fire/storage';
 
 @NgModule({
   declarations: [
@@ -16,9 +27,21 @@ import { HomePageComponent } from './home-page/home-page.component';
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
-    SharedModule
+    SharedModule,
+    provideFirebaseApp(() => initializeApp(environment.firebase)),
+    provideAnalytics(() => getAnalytics()),
+    provideAuth(() => getAuth()),
+    provideDatabase(() => getDatabase()),
+    provideFirestore(() => getFirestore()),
+    provideFunctions(() => getFunctions()),
+    provideMessaging(() => getMessaging()),
+    providePerformance(() => getPerformance()),
+    provideRemoteConfig(() => getRemoteConfig()),
+    provideStorage(() => getStorage())
   ],
-  providers: [],
+  providers: [
+    ScreenTrackingService,UserTrackingService
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- client/src/environments/environment.ts
+++ client/src/environments/environment.ts
@@ -5,13 +5,13 @@
 export const environment = {
   production: false,
   firebase: {
-    apiKey: "AIzaSyBM65M-tW9VKDwW4hDnEB7d7xZi0izpAMk",
-    authDomain: "issuetracker-efd4b.firebaseapp.com",
-    projectId: "issuetracker-efd4b",
-    storageBucket: "issuetracker-efd4b.appspot.com",
-    messagingSenderId: "1040780950775",
-    appId: "1:1040780950775:web:593de1b08d17e3f9a727f2",
-    measurementId: "G-45GKTRDMK5"
+    projectId: 'issuetracker-efd4b',
+    appId: '1:1040780950775:web:593de1b08d17e3f9a727f2',
+    storageBucket: 'issuetracker-efd4b.appspot.com',
+    apiKey: 'AIzaSyBM65M-tW9VKDwW4hDnEB7d7xZi0izpAMk',
+    authDomain: 'issuetracker-efd4b.firebaseapp.com',
+    messagingSenderId: '1040780950775',
+    measurementId: 'G-45GKTRDMK5',
   }
 };
 
