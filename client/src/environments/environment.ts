f462a8b960f397ab9f16f2f88ebdfb60da855b33
QV NGUYEN <qv.nguyen00@gmail.com>
2022-05-22 21:38:08 +0200

    fix env

--- client/src/environments/environment.ts
+++ client/src/environments/environment.ts
@@ -5,13 +5,14 @@
 export const environment = {
   production: false,
   firebase: {
-    projectId: 'issuetracker-efd4b',
-    appId: '1:1040780950775:web:593de1b08d17e3f9a727f2',
-    storageBucket: 'issuetracker-efd4b.appspot.com',
-    apiKey: 'AIzaSyBM65M-tW9VKDwW4hDnEB7d7xZi0izpAMk',
-    authDomain: 'issuetracker-efd4b.firebaseapp.com',
-    messagingSenderId: '1040780950775',
-    measurementId: 'G-45GKTRDMK5',
+    apiKey: "AIzaSyBM65M-tW9VKDwW4hDnEB7d7xZi0izpAMk",
+    authDomain: "issuetracker-efd4b.firebaseapp.com",
+    databaseURL: "https://issuetracker-efd4b-default-rtdb.europe-west1.firebasedatabase.app",
+    projectId: "issuetracker-efd4b",
+    storageBucket: "issuetracker-efd4b.appspot.com",
+    messagingSenderId: "1040780950775",
+    appId: "1:1040780950775:web:a1e4853dfc398d86a727f2",
+    measurementId: "G-KDBW3EGESZ"
   }
 };
 
